6724471df52fda27310dee0949c969e96551c934
Arpita Bhingardive <143834711+A04b@users.noreply.github.com>
2025-02-17 23:16:10 +0530

    Update dstress.sql
    Modification needed

--- dstress.sql
+++ dstress.sql
@@ -1,42 +1,11 @@
---
--- PostgreSQL database dump
---
+-- Restore DB after cloning using: 
+-- pg_restore -U postgres -d postgres -F p dstress.sql
+
 
 -- Dumped from database version 16.4
 -- Dumped by pg_dump version 16.4
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: adminpack; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION adminpack IS 'administrative functions for PostgreSQL';
-
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: activity; Type: TABLE; Schema: public; Owner: postgres
---
+-- /// NEED TO EDIT ///
 
 CREATE TABLE public.activity (
     activity_id integer NOT NULL,
